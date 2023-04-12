0b2d8e48c1a64715e4846f337f312ee35be0bb3d
AqsaIshfaq <113332730+AqsaIshfaq@users.noreply.github.com>
2023-04-12 03:06:51 -0600

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,70 @@
+/*Array to string 
+const array =["Aqsa","Waduha ","Izza "];
+  //using string constructor
+  const str=String(array)
+  console.log(str);
+  console.log (typeof str);
+  */
+  /*Join method  with commas and without commas
+const array1=["my","name","is","Aqsa"];
+const joins=array1.join('');
+const joins=array1.join(',');
+console.log(joins);
+*/
+
+/*push and pop functions 
+let array2=["Physic","Chemistry","Urdu"];
+ let str1=array2.pop("Urdu");
+ console.log("Urdu");
+ let array3=["Physic","Chemistry","Urdu"];
+ let str2=array2.push("English");
+ console.log(str2);*/
+
+ /* Shift method 
+ let num=[3,7,89,4];
+ let a=num.shift();
+ console.log(a);*/
+ /*Unshift method 
+ let num=[3,7,89,4];
+ let a=num.unshift(79);
+ console.log(num);*/
+
+ /*DElete element from an array
+ let arr=[56,90,76,45,75];
+  delete arr[4];*/
+ 
+  /*conccat function (its takes multiple array to concat )
+let arr1=[7,9,0,5,8];
+let arr2=[67,8,30,646,40];
+let array4=arr1.concat(arr2);
+console.log(array4);*/
+
+/*sorting method 
+let arr=[67,8,20,646,40];//alphabatically sort
+arr.sort();
+console.log(arr);*/
+
+//compare method  is used to  put array an in ascending order 
+/*let compare=(a,b)=>{//67-8=59 postive then not return if negative then compare it.
+    return a-b;//b-a then desending mn hojye ga 
+}
+let arr=[67,8,20,646,40];//alphabatically sort
+arr.sort(compare);//asending mn ho jye ga 
+console.log(arr);*/
+
+/*let arr=[67,8,20,646,40];
+//arr.sort(compare);
+arr.reverse
+console.log(arr);*/
+
+/*splice method 
+let arr=[67,8,20,646,40,90,700,45677];
+arr.splice(3,2,8888,9999,10000);
+console.log(arr);*/
+
+
+/*silce function 
+let arr=[67,8,20,646,40,90,700,45677];
+ let newarry=arr.slice(2,6);
+console.log(newarry);*/
+
---
+++ Strings.js
@@ -0,0 +1,30 @@
+//let n1="aqsaiShfaq"
+//let n="aqsa"
+//console.log(n1.length);//property 
+//console.log(n1.toUpperCase());//method (invoke)
+//console.log(n1.toLowerCase());
+//console.log(n.slice(2));//slice
+//console.log(n.replace("sa","uu"));//replace 
+//let n2=" girl";
+//console.log(n.concat(" is a good",n2));
+//let n="   Aqsaa     ";
+//console.log(n.trim());
+/*const str = "aqsa";
+const pa = str.padStart(10, ".");
+console.log(pa);*/
+/*const str = "aqsa";
+const padded = str.padEnd(10, ".");
+console.log(padded);*/
+/*const str = "mom ami";
+const matches = str.match(/m/g);
+console.log(matches); */
+/*const str = "Comsats university";
+const ind = str.lastIndexOf("s");
+console.log(ind);*/
+/*const str = "Aqsa ishfaq";
+const index = str.indexOf("ishfaq");
+console.log(index);*/
+
+
+
+
